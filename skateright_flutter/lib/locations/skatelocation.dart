5e308b0262570731f64c9a6752498b2a81272f54
sj0726 <123a556@naver.com>
2022-02-27 18:17:02 -0500

    integrated firebase

---
+++ skateright_flutter/lib/locations/skatelocation.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class SkateLocation extends StatefulWidget {
+  @override
+  _SkateLocationState createState() => _SkateLocationState();
+}
+
+class _SkateLocationState extends State<SkateLocation> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Skate Locations'),
+      ),
+      body: Center(
+          child: Padding(
+              padding: const EdgeInsets.only(top: 15.0),
+              child: Column(
+                children: const [],
+              ))),
+    );
+  }
+}
