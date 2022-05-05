8cd2cc48177eebd51329d82d9e203da74fa07ea3
Beck <becksons@bu.edu>
2022-05-05 15:09:39 -0400

    Changed pages a bit

--- skateright_flutter/lib/onboarding/onboarding_pgs_new.dart
+++ skateright_flutter/lib/onboarding/onboarding_pgs_new.dart
@@ -6,7 +6,49 @@ import 'package:skateright_flutter/onboarding/onboarding_pg2.dart';
 
 import 'onboarding.dart';
 void main(){
-  runApp(onBoarding5());
+  runApp(const onBoarding4());
+}
+
+MaterialPageRoute? nextPageRoute;
+
+class OnBoardPage extends StatefulWidget {
+  const OnBoardPage({Key? key, required this.nextRoute}) : super(key: key);
+  final MaterialPageRoute nextRoute;
+
+  @override
+  State<OnBoardPage> createState() => OnBoardPageState();
+}
+
+class OnBoardPageState extends State<OnBoardPage> {
+  final List<Widget> _pages = [onBoarding2(),onBoarding4(),onBoarding2()];
+  int _index = 0;
+  PageController? _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = PageController(viewportFraction: 0.95);
+    nextPageRoute = widget.nextRoute;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Theme.of(context).backgroundColor,
+      child: PageView.builder(
+        itemCount: 3,
+        controller: _controller,
+        onPageChanged: (index) => setState(() => _index = index),
+        itemBuilder: (context, i) {
+          return Padding(
+            padding:
+                const EdgeInsets.only(top: 70, bottom: 20, left: 20, right: 20),
+            child: _pages[i],
+          );
+        },
+      ),
+    );
+  }
 }
 class onBoarding4 extends StatelessWidget {
   const onBoarding4({Key? key}) : super(key: key);
@@ -14,71 +56,37 @@ class onBoarding4 extends StatelessWidget {
   Widget build(BuildContext context) {
     
     
-   return Material(
-     child: Container(
-       color: const Color.fromRGBO(20, 20, 20, 1),
-       width: MediaQuery.of(context).size.width,
-       child: Column(
+   return  Column(
          children:  [
-       
-           
-             
-           const SizedBox(height:70),
-             FittedBox(
+           const SizedBox(height:30),
+ 
+        SizedBox(
+          height: 300,
+          child:  Image.asset('assets/images/map_ratings.png')),
+        
+           const SizedBox(height: 30),
 
-           fit: BoxFit.fitWidth,
-           child: Image.asset('assets/images/map_ratings.png')),
-
-           Row(
-             children: [
-               const SizedBox(width: 50,),
-               FittedBox(
-
-
-                 child: Image.asset('assets/images/arrow_green.png',scale: .8,),
-               ),
-             ],
-           ),
-           const Padding(
-             padding: EdgeInsets.only(top:20,left:20,right:20),
-              child: Text(
-                "Tap to add new spots on the map or",
-                style: TextStyle(
-                  color: Color.fromRGBO(148, 179, 33, 1),
-                  fontFamily: 'RobotoMono',
-                  fontSize: 32,
-                  fontWeight: FontWeight.w700,
+           const SizedBox(width: 50,),
+           Image.asset('assets/images/arrow_green.png',scale: .8,),
+           const Text(
+             "Tap to add new spots on the map or",
+             style: TextStyle(
+               color: Color.fromRGBO(148, 179, 33, 1),
+               fontFamily: 'RobotoMono',
+               fontSize: 32,
+               fontWeight: FontWeight.w700,
             ),
           ),
+        const Text(
+          "review/rate \nexisting spots.",
+          style: TextStyle(
+            color: Color.fromRGBO(235, 0, 26, 1),
+            fontFamily: 'RobotoMono',
+            fontSize: 32,
+            fontWeight: FontWeight.w700,
         ),
-        Row(
-          
-          children: [
-            const Padding(
-             padding: EdgeInsets.only(left:20),
-              child: Text(
-                "review/rate \nexisting spots.",
-                style: TextStyle(
-                  color: Color.fromRGBO(235, 0, 26, 1),
-                  fontFamily: 'RobotoMono',
-                  fontSize: 32,
-                  fontWeight: FontWeight.w700,
-            ),
           ),
-        ),
-        Image.asset('assets/images/arrow_red.png',scale:.8),
-
-
-
-
-
-
-          ],
-
-
-
-
-        )
+        Image.asset('assets/images/arrow_red.png',scale:.8)
          
 
            
@@ -86,59 +94,31 @@ class onBoarding4 extends StatelessWidget {
    
    
          ],
-       ),
-   
-     ),
-   );
+       );
   }
 }
  
 class onBoarding5 extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Container(
-        color: Colors.black,
-        child: Column(children: [
+    return Column(children: [
           const SizedBox(height: 40,),
-          Row(children: [
-            const SizedBox(width: 30,),
-            Image.asset('assets/images/logocircle.png'),
-   
-             
-
-
-
-
-          ],),
+          const SizedBox(width: 30,),
+          Image.asset('assets/images/logocircle.png'),
           const SizedBox(height:40),
-          Row(
-            children: const [
-             SizedBox(width: 30,),
-              Text(
-                    "Create a profile\nand follow other \nskateboarders.",
-                    style: TextStyle(
-                      color: Color.fromRGBO(148, 179, 33, 1),
-                      fontFamily: 'RobotoMono',
-                      fontSize: 32,
-                      fontWeight: FontWeight.w700,
-                ),
-              ),
-            ],
-          ),
+          SizedBox(width: 30,),
+           Text(
+                 "Create a profile\nand follow other \nskateboarders.",
+                 style: TextStyle(
+                   color: Color.fromRGBO(148, 179, 33, 1),
+                   fontFamily: 'RobotoMono',
+                   fontSize: 32,
+                   fontWeight: FontWeight.w700,
+             ),
+           ),
           const SizedBox(height: 80),
           Image.asset('assets/images/profile_example.png',scale: 1.8)
 
-        ]),
-        
-      
-      ),
-
-    );
-   
+        ]);
   }
-
-
-
-  
 }
